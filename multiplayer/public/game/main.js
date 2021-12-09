5ac6754c221d276a1a5d39ace6c48ebf1f01c33b
NikitaLitvishko <litvishko.nikita@mail.ru>
2021-12-09 19:52:44 +0200

    adding full screen

--- multiplayer/public/game/main.js
+++ multiplayer/public/game/main.js
@@ -16,7 +16,7 @@ const BALLSIZE = 7;
 
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
   for(var i =0; i<50; i++) {
     var x = random(-width*MAPSIZE, width*MAPSIZE);
@@ -67,6 +67,7 @@ function setup() {
 }
 
 function draw() {
+
   background(0);
   //мы перемещаем начало нашего холста, чтобы наша капля всегда была в центре
   translate(width / 2, height / 2);
