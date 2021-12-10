338dbf150955dd71af80a87deeb51d9e60ff2e4a
NikitaLitvishko <litvishko.nikita@mail.ru>
2021-12-10 19:19:29 +0200

    gotfix

--- multiplayer/server.js
+++ multiplayer/server.js
@@ -81,7 +81,7 @@ function newConnection(socket) {
                 console.log("Disconnection from : "+clients[i].name + " "+id);
                 io.to(id).emit('forceDisconnect');
                 clients.splice(clients[id], 1);
-                once = false;
+                
             }
         }
          once = true;
